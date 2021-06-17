578a49b11ed0e569fedb6b9f913920e5e115aa5f
Felipe <alphilipe@gmail.com>
2021-06-17 04:39:57 -0400

    Se añadio un plantilla

--- firmas-manuscritas/app/rutas.py
+++ firmas-manuscritas/app/rutas.py
@@ -1,5 +1,11 @@
 from app import app
+from flask import Flask, render_template
+
 @app.route('/')
-@app.route('/index')
+@app.route('/home')
+def home():
+    return "BienBenido a home"
+
+@app.route('/index/')
 def index():
-    return "HOLA"
+    return render_template('index.html')
